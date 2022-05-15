17add255dabf658359d140ddb306a655f87519b1
TianC Wang <thewater2016@qq.com>
2022-05-15 12:23:19 +0800

    ISaveLoad #1

---
+++ Assets/Scripts/InfChunk/ISaveLoad.cs
@@ -0,0 +1,19 @@
+namespace InfChunk
+{
+    /// <summary>
+    /// An interface provides saving to bytes and loading from bytes operations.
+    /// </summary>
+    public interface ISaveLoad
+    {
+        /// <summary>
+        /// Save the data to bytes.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        void SaveData(out byte[] data);
+        /// <summary>
+        /// Load the data from bytes.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        void LoadData(byte[] data);
+    }
+}
